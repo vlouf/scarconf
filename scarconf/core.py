6a72afac40357322d0304f326c4e14ec31c853de
Valentin Louf <valentin.louf@bom.gov.au>
2021-06-30 15:28:07 +1000

    first commit

---
+++ scarconf/core.py
@@ -0,0 +1,93 @@
+"""
+S3CAR Global configuration class.
+"""
+__authors__ = "Valentin Louf"
+__contact__ = "valentin.louf@bom.gov.au"
+__version__ = "0.5.0"
+__date__ = "2021/06"
+
+import os
+import time
+
+import pandas as pd
+
+
+class S3car():
+    def __init__(self, root_dir="/srv/data/s3car-server", etc_dir="/etc/opt/s3car-server/", html_dir="/srv/web/s3car-server/www") -> None:
+        self.cluttercal_path = os.path.join(root_dir, "cluttercal")
+        self.config_path = os.path.join(root_dir, "config")
+        self.dualpolqc_path = os.path.join(root_dir, "dualpol_qc")
+        self.gpmmatch_path = os.path.join(root_dir, "gpmmatch")
+        self.html_path = html_dir
+        self.log_path = os.path.join(root_dir, "log")
+        self.solar_path = os.path.join(root_dir, "solar", "data")
+        self.vols_path = os.path.join(root_dir, "vols")
+        self.zdr_path = os.path.join(root_dir, "zdr_monitoring")
+
+        self.tier1_radars = [2, 3, 4, 8, 19, 22, 23, 24, 28, 40, 52, 63, 64, 66, 68, 70, 71, 73, 76]
+
+        self.zdr_range = (-2, 2)  # ZDR min/max for histogram.
+        self.zdr_step = abs(0.08)  # ZDR resolution in dB
+        self.zdr_bins = int(round((self.zdr_range[1] - self.zdr_range[0]) / self.zdr_step))
+
+        self.check_paths_exist()
+        self.set_radar_site_info()
+
+    def check_paths_exist(self):
+        for k, v in self.__dict__.items():
+            if "path" in k:
+                if not os.path.exists(v):
+                    raise FileNotFoundError(f"Directory {v} not found.")
+
+    def get_lat(self, rid: int) -> float:
+        """
+        Get latitude for given radar ID.
+        Parameter:
+        ==========
+        rid: int
+            Radar Rapic ID
+        Returns:
+        ========
+        latitude: float
+            Radar site latitude
+        """
+        return self.radar_site_info.loc[self.radar_site_info.id == rid].site_lat.values[0]
+
+    def get_lon(self, rid: int) -> float:
+        """
+        Get longitude for given radar ID.
+        Parameter:
+        ==========
+        rid: int
+            Radar Rapic ID
+        Returns:
+        ========
+        longitude: float
+            Radar site longitude
+        """
+        return self.radar_site_info.loc[self.radar_site_info.id == rid].site_lon.values[0]
+
+    def set_radar_site_info(self):
+        radar_fname = os.path.join(self.config_path, "radar_site_list.csv")
+        self.radar_site_info = pd.read_csv(radar_fname)
+        if len(self.radar_site_info) == 0:
+            raise ValueError(f"Invalid radar configuration file: {radar_fname}. Exiting code.")
+
+
+class Chronos:
+    """
+    https://www.youtube.com/watch?v=QcHvzNBtlOw
+    """
+
+    def __init__(self, messg=None):
+        self.messg = messg
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, ntype, value, traceback):
+        self.time = time.time() - self.start
+        if self.messg is not None:
+            print(f"{self.messg} took {self.time:.2f}s.")
+        else:
+            print(f"Processed in {self.time:.2f}s.")
---
+++ setup.py
@@ -0,0 +1,40 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="scarconf",
+    version="0.0.1",
+    description="S3car global parameters.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vlouf/swirlconf",
+    author="Valentin Louf",
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[  # Optional
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="radar weather meteorology correction",
+    packages=find_packages(exclude=["notebook"]),
+    install_requires=["pandas"],
+    project_urls={"Source": "https://github.com/vlouf/scarconf/",},
+)
