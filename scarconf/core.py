1ca9631a80e8a1775012bffab8c86bdbe6a1b5a5
Valentin Louf <valentin.louf@bom.gov.au>
2024-04-10 10:30:37 +1000

    Move dashboard misc into scarconf

--- scarconf/core.py
+++ scarconf/core.py
@@ -1,9 +1,10 @@
 """
 S3CAR Global configuration class.
 """
+
 __authors__ = "Valentin Louf"
 __contact__ = "valentin.louf@bom.gov.au"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __date__ = "2024/04/03"
 
 import configparser
@@ -12,16 +13,18 @@ import math
 import os
 import time
 
+import numpy as np
 import pandas as pd
 
 
-class S3car():
-    def __init__(self,
-                 root_dir="/srv/data/s3car-server",
-                 etc_dir="/etc/opt/s3car-server/",
-                 html_dir="/srv/web/s3car-server/www",
-                 read_radars=True,
-                 ) -> None:
+class S3car:
+    def __init__(
+        self,
+        root_dir="/srv/data/s3car-server",
+        etc_dir="/etc/opt/s3car-server/",
+        html_dir="/srv/web/s3car-server/www",
+        read_radars=True,
+    ) -> None:
         self.root_path = root_dir
         self.cluttercal_path = os.path.join(root_dir, "cluttercal")
         self.config_path = os.path.join(root_dir, "config")
@@ -47,13 +50,13 @@ class S3car():
         self.gpm_requestid = "001"
         self.rainptl_enabled = True
         self.region = {
-            'lat': (-46.0, -7.5),
-            'lon': (112.0, 155.0),
+            "lat": (-46.0, -7.5),
+            "lon": (112.0, 155.0),
         }
         # TODO: consider having per-radar.tier instead of exhaustive list here
         self.tier1_radars = [2, 3, 4, 8, 19, 22, 23, 24, 28, 40, 52, 63, 64, 66, 68, 70, 71, 73, 76]
-        self.tier2_radars = [1, 5, 6, 7, 14, 15, 16, 17, 29, 31, 32, 38, 42, 46, 49, 50, 55, 58, 67,
-                             69, 72, 74, 75, 77, 78, 79, 93, 94, 95, 96, 98, 107, 108, 109, 110]
+        self.tier2_radars = [1, 5, 6, 7, 14, 15, 16, 17, 29, 31, 32, 38, 42, 46, 49, 50, 55, 58]
+        self.tier2_radars.extend([67, 69, 72, 74, 75, 77, 78, 79, 93, 94, 95, 96, 98, 107, 108, 109, 110])
         self.tier3_radars = [9, 10, 25, 26, 27, 30, 33, 36, 37, 39, 41, 44, 48, 53, 54, 56, 62, 97]
         self.censor_radars = [9, 30]
         self.nosun_radars = [6, 31, 32, 48, 95]
@@ -70,6 +73,104 @@ class S3car():
             if "path" in k:
                 if not os.path.exists(v):
                     raise FileNotFoundError(f"Directory {v} not found.")
+
+    def get_amber_alert(kpi: int) -> str:
+        """
+        Get the Green, Amber, or red alert
+
+        Parameters:
+        ===========
+        kpir: int
+            KPI index.
+
+        Returns:
+        ========
+        alert: str
+            Color (HTML) level.
+        """
+        green = "#238823"
+        amber = "#FFBF00"
+        red = "#D2222D"
+        white = "#FFFFFF"
+        if kpi == 0:  # Undefined
+            return white
+        elif kpi == 1:
+            return green
+        elif kpi == 2:
+            return amber
+        elif kpi == 3:
+            return red
+
+    def get_kpi_pointing(elev: float, azimuth: float = None) -> int:
+        """
+        Get the KPI index for the pointing accuracy.
+
+        Parameters:
+        ===========
+        elev: float
+            Elevation pointing error.
+        azimuth: float
+            Azimuth pointing error.
+
+        Returns:
+        ========
+        kpi: int
+            Green (1), Amber (2) or Red (3) alert.
+        """
+        if elev is None:
+            angle = None
+        else:
+            if azimuth is None:
+                azimuth = 0
+            angle = np.max([np.abs(elev), np.abs(azimuth)])
+
+        if angle is None:
+            return 0
+        if np.isnan(angle):
+            return 0
+
+        if np.abs(angle) <= 0.1:
+            return 1
+        elif np.abs(angle) <= 0.2:
+            return 2
+        else:
+            return 3
+
+    def get_kpi_reflectivity(tier: int, offset: float) -> int:
+        """
+        Get the KPI index for the given radar.
+
+        Parameters:
+        ===========
+        tier: int
+            Radar tier (1, 2, 3)
+        offset: float
+            Radar reflectivity offset (dB)
+
+        Returns:
+        ========
+        kpi: int
+            Green (1), Amber (2) or Red (3) alert.
+        """
+        if offset is None:
+            return 0
+        if np.isnan(offset):
+            return 0
+
+        if tier == 1:
+            if np.abs(offset) < 1:
+                return 1
+            elif np.abs(offset) >= 1 and np.abs(offset) < 2:
+                return 2
+            else:
+                return 3
+        else:
+            if np.abs(offset) < 3:
+                return 1
+            elif np.abs(offset) >= 3 and np.abs(offset) < 5:
+                return 2
+            else:
+                return 3
 
     def get_lat(self, rid: int) -> float:
         """
@@ -132,9 +233,9 @@ class S3car():
         # TODO: vary according to config
 
         # add time to log_fmt if running in apptainer/singularity
-        log_fmt="%(levelname)s %(message)s"
-        if os.environ.keys() & {'APPTAINER_CONTAINER', 'SINGULARITY_CONTAINER'}:
-            log_fmt="%(asctime)s " + log_fmt
+        log_fmt = "%(levelname)s %(message)s"
+        if os.environ.keys() & {"APPTAINER_CONTAINER", "SINGULARITY_CONTAINER"}:
+            log_fmt = "%(asctime)s " + log_fmt
         logging.basicConfig(
             level=level,
             format=log_fmt,
@@ -144,15 +245,21 @@ class S3car():
     def calc_region(self):
         # for converting lat,lon pairs use pyproj.Transformer.from_crs("EPSG:4326", "EPSG:3857")
         eq_radius = 6378137
-        def delta(t): return t[1] - t[0]
-        def lon2mx(lon): return eq_radius * math.radians(lon)
-        def lat2my(lat): return eq_radius * math.log(math.tan(math.pi / 4 + math.radians(lat) / 2))
+
+        def delta(t):
+            return t[1] - t[0]
+
+        def lon2mx(lon):
+            return eq_radius * math.radians(lon)
+
+        def lat2my(lat):
+            return eq_radius * math.log(math.tan(math.pi / 4 + math.radians(lat) / 2))
 
         # derived region vals used in dashboard / make_bokeh_map()
-        self.region['mercator_x'] = tuple(lon2mx(lon) for lon in self.region['lon'])
-        self.region['mercator_y'] = tuple(lat2my(lat) for lat in self.region['lat'])
-        aspect_xy = delta(self.region['mercator_x']) / delta(self.region['mercator_y'])
-        self.region['aspect_ratio'] = aspect_xy
+        self.region["mercator_x"] = tuple(lon2mx(lon) for lon in self.region["lon"])
+        self.region["mercator_y"] = tuple(lat2my(lat) for lat in self.region["lat"])
+        aspect_xy = delta(self.region["mercator_x"]) / delta(self.region["mercator_y"])
+        self.region["aspect_ratio"] = aspect_xy
 
     def read_local_config(self, etc_dir):
         """Read local configuration settings (if any)."""
@@ -169,36 +276,36 @@ class S3car():
         # use local config #
 
         # [country]
-        if have_config(cfg, 'country', 'code'):
-            self.country_code = cfg['country']['code']
+        if have_config(cfg, "country", "code"):
+            self.country_code = cfg["country"]["code"]
         # [gpm]
-        if have_config(cfg, 'gpm', 'userid'):
-            self.gpm_userid = cfg['gpm']['userid']
-        if have_config(cfg, 'gpm', 'requestid'):
-            self.gpm_requestid = cfg['gpm']['requestid']
+        if have_config(cfg, "gpm", "userid"):
+            self.gpm_userid = cfg["gpm"]["userid"]
+        if have_config(cfg, "gpm", "requestid"):
+            self.gpm_requestid = cfg["gpm"]["requestid"]
         # [rainptl]
-        if have_config(cfg, 'rainptl', 'enable'):
-            self.rainptl_enabled = cfg['rainptl'].getboolean('enable')
+        if have_config(cfg, "rainptl", "enable"):
+            self.rainptl_enabled = cfg["rainptl"].getboolean("enable")
         # [radar]
-        if have_config(cfg, 'radar', 'tier1'):
-            self.tier1_radars = config_list(cfg, int, 'radar', 'tier1')
-        if have_config(cfg, 'radar', 'tier2'):
-            self.tier2_radars = config_list(cfg, int, 'radar', 'tier2')
-        if have_config(cfg, 'radar', 'tier3'):
-            self.tier3_radars = config_list(cfg, int, 'radar', 'tier3')
-        if have_config(cfg, 'radar', 'censor'):
-            self.censor_radars = config_list(cfg, int, 'radar', 'censor')
-        if have_config(cfg, 'radar', 'nosun'):
-            self.nosun_radars = config_list(cfg, int, 'radar', 'nosun')
+        if have_config(cfg, "radar", "tier1"):
+            self.tier1_radars = config_list(cfg, int, "radar", "tier1")
+        if have_config(cfg, "radar", "tier2"):
+            self.tier2_radars = config_list(cfg, int, "radar", "tier2")
+        if have_config(cfg, "radar", "tier3"):
+            self.tier3_radars = config_list(cfg, int, "radar", "tier3")
+        if have_config(cfg, "radar", "censor"):
+            self.censor_radars = config_list(cfg, int, "radar", "censor")
+        if have_config(cfg, "radar", "nosun"):
+            self.nosun_radars = config_list(cfg, int, "radar", "nosun")
         # [region]
-        if have_config(cfg, 'region', 'lat'):
-            self.region['lat'] = tuple(config_list(cfg, float, 'region', 'lat'))
-        if have_config(cfg, 'region', 'lon'):
-            self.region['lon'] = tuple(config_list(cfg, float, 'region', 'lon'))
+        if have_config(cfg, "region", "lat"):
+            self.region["lat"] = tuple(config_list(cfg, float, "region", "lat"))
+        if have_config(cfg, "region", "lon"):
+            self.region["lon"] = tuple(config_list(cfg, float, "region", "lon"))
 
         # individual radar info [radar.ID]
-        if have_config(cfg, 'radar', 'ids'):
-            radar_ids = config_list(cfg, int, 'radar', 'ids')
+        if have_config(cfg, "radar", "ids"):
+            radar_ids = config_list(cfg, int, "radar", "ids")
             for rid in radar_ids:
                 if not self.get_radar_tier(rid):
                     print(f"No tier for radar {rid} configured")
@@ -206,8 +313,8 @@ class S3car():
                 if not stanza in cfg:
                     print(f"No configuration {stanza} entry found")
                     continue
-                if have_config(cfg, stanza, 'longname'):
-                    self.radar_longname[rid] = cfg[stanza]['longname']
+                if have_config(cfg, stanza, "longname"):
+                    self.radar_longname[rid] = cfg[stanza]["longname"]
 
     def get_radar_longname(self, rid: int) -> str:
         """
@@ -231,90 +338,94 @@ class S3car():
     def _init_longname_default(self):
         """Initialise longname map."""
         self.radar_longname = {
-        1: "Melbourne (Broadmeadows)",
-        2: "Melbourne",
-        3: "Wollongong (Appin)",
-        4: "Newcastle",
-        5: "Carnarvon",
-        6: "Geraldton",
-        7: "Wyndham",
-        8: "Gympie (Mt Kanigan)",
-        9: "Gove",
-        10: "Darwin Airport",
-        14: "Mt Gambier",
-        15: "Dampier",
-        16: "Pt Hedland",
-        17: "Broome",
-        19: "Cairns",
-        22: "Mackay",
-        23: "Gladstone",
-        24: "Bowen",
-        25: "Alice Springs",
-        26: "Perth Airport",
-        27: "Woomera",
-        28: "Grafton",
-        29: "Learmonth",
-        30: "Mildura",
-        31: "Albany",
-        32: "Esperance",
-        33: "Ceduna",
-        36: "Gulf of Carpentaria (Mornington Is)",
-        37: "Hobart Airport",
-        38: "Newdegate",
-        39: "Halls Creek",
-        40: "Canberra (Captains Flat)",
-        41: "Willis Island",
-        42: "Katherine (Tindal)",
-        44: "Giles",
-        46: "Adelaide (Sellicks Hill)",
-        48: "Kalgoorlie",
-        49: "Yarrawonga",
-        50: "Brisbane (Marburg)",
-        52: "N.W. Tasmania (West Takone)",
-        53: "Moree",
-        54: "Sydney (Kurnell)",
-        55: "Wagga Wagga",
-        56: "Longreach",
-        58: "South Doodlakine",
-        62: "Norfolk Island",
-        63: "Darwin (Berrimah)",
-        64: "Adelaide (Buckland Park)",
-        66: "Brisbane (Mt Stapylton)",
-        67: "Warrego",
-        68: "Bairnsdale",
-        69: "Namoi (Blackjack Mountain)",
-        70: "Perth (Serpentine)",
-        71: "Sydney (Terrey Hills)",
-        72: "Emerald",
-        73: "Townsville (Hervey Range)",
-        74: "Greenvale",
-        75: "Mount Isa",
-        76: "Hobart (Mt Koonya)",
-        77: "Warruwi",
-        78: "Weipa",
-        79: "Watheroo",
-        93: "Brewarrina",
-        94: "Hillston",
-        95: "Rainbow (Wimmera)",
-        96: "Yeoval",
-        97: "Mildura",
-        98: "Taroom",
-        105: "Brisbane Airport",
-        107: "Richmond",
-        108: "Darling Downs",
-        109: "Goondiwindi",
-        110: "Tennant Creek",
-    }
+            1: "Melbourne (Broadmeadows)",
+            2: "Melbourne",
+            3: "Wollongong (Appin)",
+            4: "Newcastle",
+            5: "Carnarvon",
+            6: "Geraldton",
+            7: "Wyndham",
+            8: "Gympie (Mt Kanigan)",
+            9: "Gove",
+            10: "Darwin Airport",
+            14: "Mt Gambier",
+            15: "Dampier",
+            16: "Pt Hedland",
+            17: "Broome",
+            19: "Cairns",
+            22: "Mackay",
+            23: "Gladstone",
+            24: "Bowen",
+            25: "Alice Springs",
+            26: "Perth Airport",
+            27: "Woomera",
+            28: "Grafton",
+            29: "Learmonth",
+            31: "Albany",
+            32: "Esperance",
+            33: "Ceduna",
+            36: "Gulf of Carpentaria (Mornington Is)",
+            37: "Hobart Airport",
+            38: "Newdegate",
+            39: "Halls Creek",
+            40: "Canberra (Captains Flat)",
+            41: "Willis Island",
+            42: "Katherine (Tindal)",
+            44: "Giles",
+            46: "Adelaide (Sellicks Hill)",
+            48: "Kalgoorlie",
+            49: "Yarrawonga",
+            50: "Brisbane (Marburg)",
+            52: "N.W. Tasmania (West Takone)",
+            53: "Moree",
+            54: "Sydney (Kurnell)",
+            55: "Wagga Wagga",
+            56: "Longreach",
+            58: "South Doodlakine",
+            62: "Norfolk Island",
+            63: "Darwin (Berrimah)",
+            64: "Adelaide (Buckland Park)",
+            66: "Brisbane (Mt Stapylton)",
+            67: "Warrego",
+            68: "Bairnsdale",
+            69: "Namoi (Blackjack Mountain)",
+            70: "Perth (Serpentine)",
+            71: "Sydney (Terrey Hills)",
+            72: "Emerald",
+            73: "Townsville (Hervey Range)",
+            74: "Greenvale",
+            75: "Mount Isa",
+            76: "Hobart (Mt Koonya)",
+            77: "Warruwi",
+            78: "Weipa",
+            79: "Watheroo",
+            93: "Brewarrina",
+            94: "Hillston",
+            95: "Rainbow (Wimmera)",
+            96: "Yeoval",
+            97: "Mildura",
+            98: "Taroom",
+            105: "Brisbane Airport",
+            107: "Richmond",
+            108: "Darling Downs",
+            109: "Goondiwindi",
+            110: "Tennant Creek",
+            112: "Gove",
+            114: "Carnarvon",
+        }
+
 
 def have_config(cfg, group, item):
     """Return True if cfg[group][item] exists."""
     return cfg and group in cfg and item in cfg[group]
+
 
 def config_list(cfg, ty, group, item):
     """Return cfg[group][item] as list of type `ty`."""
     if not cfg[group][item]:
         return []
     return [ty(r) for r in cfg[group][item].split()]
+
 
 class Chronos:
     """
